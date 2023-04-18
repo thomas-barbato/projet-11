e7af15acf7b5ea913d43b4a9f6880041a8a144ba
Case Gibson <t.barbato78@gmail.com>
2023-04-18 13:51:10 +0200

    add:test_logout

---
+++ tests/test_logout.py
@@ -0,0 +1,10 @@
+import pytest
+import server
+
+
+server.app.config["TESTING"] = True
+client = server.app.test_client()
+
+def test_if_url_redirect_after_logout():
+    response = client.get("/logout")
+    assert response.status_code == 302
