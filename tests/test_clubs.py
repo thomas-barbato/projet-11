45e21f383bf98973fee60694757e7c9088b3a2c0
Case Gibson <t.barbato78@gmail.com>
2023-04-18 13:46:56 +0200

    add:test_clubs.py

---
+++ tests/test_clubs.py
@@ -0,0 +1,26 @@
+import pytest
+import server
+
+
+server.app.config["TESTING"] = True
+client = server.app.test_client()
+
+
+@pytest.fixture
+def clubs():
+    yield server.clubs
+
+
+def test_check_if_a_club_email_exists(clubs):
+    response = client.post("/showSummary", data={
+        'email': "admin@irontemple.com"
+    })
+    assert response.status_code == 200
+    assert b"admin@irontemple.com" in response.data
+
+
+def test_check_if_a_club_email_does_not_exists(clubs):
+    response = client.post("/showSummary", data={
+        'email': "admin@irontemple.com"
+    })
+    assert response.data not in clubs
