0208ce7d61cd4646f640426f3e93ab7e6755d178
Case Gibson <t.barbato78@gmail.com>
2023-04-19 12:37:09 +0200

    fix:reorder functions in tests directory

--- tests/test_logout.py
+++ tests/test_logout.py
@@ -5,6 +5,7 @@ import server
 server.app.config["TESTING"] = True
 client = server.app.test_client()
 
+
 def test_if_url_redirect_after_logout():
     response = client.get("/logout")
     assert response.status_code == 302
